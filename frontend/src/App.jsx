e6033015ac0bf84fae00e2aad36eee46d9f51556
Subhasish <me.subhasish54@gmail.com>
2026-04-07 22:14:09 +0530

    Initial project setup with backend and frontend structure with README file

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <div className="center">
+          Salary Management System
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
